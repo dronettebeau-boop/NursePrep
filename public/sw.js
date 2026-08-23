3f2850d8662806ed7d65e9db68011fe8bfa8c66e
dronettebeau-boop <dronettebeau@gmail.com>
2026-08-23 13:09:12 -0500

    Fix mobile logo, safe areas, and navigation

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='nurseprep-shell-v6';
+const CACHE='nurseprep-shell-v9';
 const SHELL=['/','/index.html','/courses.html','/scanner.html','/assets/styles.css','/assets/common.js','/manifest.webmanifest','/assets/icon-192.png','/assets/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
